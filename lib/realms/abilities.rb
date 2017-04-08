2d64ad40d8408c877624c41cdc924a98c0a37c66
Tony Schneider <tonywok@gmail.com>
2017-04-08 10:39:17 -0400

    auto require some directories
    it was getting annoying

--- lib/realms/abilities.rb
+++ lib/realms/abilities.rb
@@ -1,19 +1,8 @@
 require "realms/abilities/ability"
-require "realms/abilities/optional"
-require "realms/abilities/multi"
-require "realms/abilities/choose"
-require "realms/abilities/trade"
-require "realms/abilities/combat"
-require "realms/abilities/draw"
-require "realms/abilities/scrap_card_from_trade_row"
-require "realms/abilities/destroy_target_base"
-require "realms/abilities/acquire_ship_and_top_deck"
-require "realms/abilities/authority"
-require "realms/abilities/top_deck_next_ship"
-require "realms/abilities/scrap_from_hand_or_discard_pile"
-require "realms/abilities/copy_ship"
-require "realms/abilities/draw_then_scrap_from_hand"
-require "realms/abilities/discard"
+
+Dir[File.expand_path('../abilities/*.rb', __FILE__)].each do |file|
+  require file
+end
 
 module Realms
   module Abilities
--- lib/realms/actions.rb
+++ lib/realms/actions.rb
@@ -1,12 +1,8 @@
 require "realms/actions/action"
-require "realms/actions/end_main_phase"
-require "realms/actions/attack"
-require "realms/actions/scrap_ability"
-require "realms/actions/ally_ability"
-require "realms/actions/play_card"
-require "realms/actions/base_ability"
-require "realms/actions/acquire_card"
-require "realms/actions/discard"
+
+Dir[File.expand_path('../actions/*.rb', __FILE__)].each do |file|
+  require file
+end
 
 module Realms
   module Actions
